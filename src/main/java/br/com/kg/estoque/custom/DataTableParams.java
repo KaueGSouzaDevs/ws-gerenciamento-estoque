22395d8645b63545b04e91dea38f3baba0610404
Kauê Gallego <kauegallegosouza@gmail.com>
2025-05-23 01:11:41 -0300

    Refatora classes DataTableParams e DataTableResult para melhorar a estrutura e legibilidade. A classe DataTableParams foi transformada de um record para uma classe com atributos e métodos getters/setters, enquanto a DataTableResult teve suas propriedades renomeadas para seguir a convenção do jQuery DataTable. Ajustes visam facilitar a manipulação e a apresentação dos dados na tabela.

--- src/main/java/br/com/kg/estoque/custom/DataTableParams.java
+++ src/main/java/br/com/kg/estoque/custom/DataTableParams.java
@@ -1,22 +1,57 @@
 package br.com.kg.estoque.custom;
 
-    /**
-     * Retorna uma lista de entidades que correspondem aos critérios de pesquisa especificados.
-     * 
-     * @param colunas As colunas nas quais a pesquisa será realizada.
-     * @param sSearch O termo de pesquisa.
-     * @param iSortCol_0 O índice da coluna pela qual os resultados serão ordenados.
-     * @param sSortDir_0 A direção da ordenação (ascendente ou descendente).
-     * @param iDisplayStart O índice do primeiro resultado a ser retornado.
-     * @param iDisplayLength O número máximo de resultados a serem retornados.
-     * @param type O tipo de entidade.
-     * @return Uma lista de entidades que correspondem aos critérios de pesquisa especificados.
-     */
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-public record DataTableParams(
-    Integer iDisplayStart,
-    Integer iDisplayLength,
-    String sSortDir_0,
-    Integer iSortCol_0,
-    String sSearch) {
+
+
+/*
+ * Classe DataTableParams
+ * 
+ * Essa classe é utilizada para armazenar parâmetros de configuração para uma tabela de dados. Ela é projetada para ser flexível e permitir a customização da tabela de acordo com as necessidades do usuário.
+ * 
+ * Atributos
+ * 
+ * draw: Esse atributo é utilizado para controlar a renderização da tabela. Ele pode ser utilizado para evitar que a tabela seja renderizada várias vezes desnecessariamente.
+ * start: Esse atributo define o índice inicial da lista de dados que deve ser exibida na tabela.
+ * length: Esse atributo define o número de registros que devem ser exibidos na tabela.
+ * searchValue: Esse atributo é utilizado para armazenar o valor de pesquisa digitado pelo usuário. Ele pode ser utilizado para filtrar os dados da tabela.
+ * orderCol: Esse atributo define a coluna que deve ser utilizada para ordenar os dados da tabela.
+ * orderDir: Esse atributo define a direção da ordenação (ascendente ou descendente).
+ * 
+ * @Author Kauê Gallego de Souza
+ */
+@ToString
+public class DataTableParams {
+
+    public DataTableParams() {
+    }
+
+    public DataTableParams(String draw, Integer start, Integer length, String searchValue, Integer orderCol, String orderDir) {
+        this.draw = draw;
+        this.start = start;
+        this.length = length;
+        this.searchValue = searchValue;
+        this.orderCol = orderCol;
+        this.orderDir = orderDir;
+    }
+
+    @Getter @Setter
+    private String draw;
+
+    @Getter @Setter
+    private Integer start;
+    
+    @Getter @Setter
+    private Integer length;
+
+    @Getter @Setter
+    private String searchValue;
+
+    @Getter @Setter
+    private Integer orderCol;
+
+    @Getter @Setter
+    private String orderDir;
 }
--- src/main/java/br/com/kg/estoque/custom/DataTableResult.java
+++ src/main/java/br/com/kg/estoque/custom/DataTableResult.java
@@ -1,34 +1,31 @@
 package br.com.kg.estoque.custom;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * ?Classe responsável por popular o as informações que serão exibidas no json de acordo
- * ?com a estrutura padrão da biblioteca jQuery DataTable  
- * @author Ailton
- * @version 2.0.1
- */
 @JsonSerialize
 public class DataTableResult {
-	
-	@Getter @Setter
-	@JsonProperty("sEcho")
-	private String sEcho;
-    
-	@Getter @Setter
-	@JsonProperty("iTotalRecords")
-	private Integer iTotalRecords;
-    
-	@Getter @Setter
-	@JsonProperty("iTotalDisplayRecords")
-	private Long iTotalDisplayRecords;
-    
-	@Getter @Setter
-	@JsonProperty("aaData")
-	private Object[] aaData;
 
+	@Getter @Setter
+	@JsonProperty("draw")
+	private String draw;
+
+	@Getter @Setter
+	@JsonProperty("recordsTotal")
+	private Integer recordsTotal;
+
+	@Getter @Setter
+	@JsonProperty("recordsFiltered")
+	private Long recordsFiltered;
+
+	@Getter @Setter
+	@JsonProperty("data")
+	private List<Object[]> data;
+
+  // getters e setters omitidos para brevidade
 }
